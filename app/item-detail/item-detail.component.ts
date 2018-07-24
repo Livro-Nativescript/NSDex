60c3558b558526b2afb6e600eac208873f8f064a
Bruno Codeman <brunobemfica@gmail.com>
2018-07-24 00:20:26 -0300

    initial commit

---
+++ app/item-detail/item-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DataService, IDataItem } from "../core/data.service";
+
+@Component({
+    selector: "ItemDetail",
+    moduleId: module.id,
+    templateUrl: "./item-detail.component.html",
+    styleUrls: ["~/../../custom.css"]
+})
+export class ItemDetailComponent implements OnInit {
+    item: IDataItem;
+
+    constructor(
+        private data: DataService,
+        private route: ActivatedRoute
+    ) { }
+
+    ngOnInit(): void {
+        const id = +this.route.snapshot.params.id;
+        this.item = this.data.getItem(id);
+    }
+}
